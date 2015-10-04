b1ec714f283cde1089fa3975ae5c8eaa73403e2d
ognestraz <ognestraz@gmail.com>
2015-10-04 13:47:07 +0300

    Middleware admin.auth

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,10 @@
 <?php
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Ognestraz\Admin\Http\Controllers'], function () {
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Ognestraz\Admin\Http\Controllers',
+    'middleware' => 'auth.admin'
+    ], function () {
 
     Route::any('/site/tree', array('uses' => 'SiteController@tree'));
     Route::any('/site/create/{id}', array('uses' => 'SiteController@create'));    
