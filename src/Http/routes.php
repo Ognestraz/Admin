0e16fa0144875d0ddeac3f8e5b7bd0252b8352b7
ognestraz <ognestraz@gmail.com>
2015-10-04 14:11:37 +0300

    Fix routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,14 +2,17 @@
 
 Route::group([
     'prefix' => 'admin',
-    'namespace' => 'Ognestraz\Admin\Http\Controllers',
-    'middleware' => 'auth.admin'
+    'namespace' => 'Ognestraz\Admin\Http\Controllers'
     ], function () {
 
-    Route::any('/site/tree', array('uses' => 'SiteController@tree'));
-    Route::any('/site/create/{id}', array('uses' => 'SiteController@create'));    
+    Route::group(['middleware' => 'auth.admin'], function () {
+        Route::any('/site/tree', array('uses' => 'SiteController@tree'));
+        Route::any('/site/create/{id}', array('uses' => 'SiteController@create'));    
+
+        Route::resource('/site', 'SiteController');
+        Route::any('/logout', array('uses' => 'MainController@logout'));
+    });
     
-    Route::resource('/site', 'SiteController');
     Route::any('/', array('uses' => 'MainController@index'));
-    Route::any('/logout', array('uses' => 'MainController@logout'));
+
 });
